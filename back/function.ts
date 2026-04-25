01e5cc18235a1d63960201cb549d08398af4e145
Gabriel Ramalho <gabrieldelai11ramalho@gmail.com>
2026-04-25 00:39:17 -0300

    f

--- back/function.ts
+++ back/function.ts
@@ -2,11 +2,15 @@ import type { Config, Context } from "@netlify/functions";
 import { getStore } from "@netlify/blobs";
 
 export default async (req: Request, context: Context) => {
+    console.log("BEGIN");
     const store = getStore("data");
     let data = await store.get("data", { type: "text" });
+    console.log(data);
     data ??= "0";
     data = Number(data) + 1 + "";
+    console.log(data);
     await store.set("data", data);
+    console.log("END");
     return new Response("hello, world!\n" + data);
 };
 
