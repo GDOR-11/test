0da0513507e455dec14f9070f0d6831d33525ac7
Gabriel Ramalho <gabrieldelai11ramalho@gmail.com>
2026-04-25 00:35:11 -0300

    d

--- back/function.ts
+++ back/function.ts
@@ -1,9 +1,14 @@
 import type { Config, Context } from "@netlify/functions";
 import fs from "fs/promises";
+import { getStore } from "@netlify/blobs";
 
 export default async (req: Request, context: Context) => {
-    const files = await fs.readdir(".");
-    return new Response("hello, world!\n" + files.join(","));
+    const store = getStore("data");
+    let data = await store.get("data", { type: "text" });
+    data ??= "0";
+    data = Number(data) + 1 + "";
+    await store.set("data", data);
+    return new Response("hello, world!\n" + data);
 };
 
 export const config: Config = {
